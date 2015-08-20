08cc7dcfb6f8d061e300720880e2b8579d27412e
Anthony Ha <anthonyha96@gmail.com>
2015-08-19 17:50:44 -0700

    Add toString and fromString methods to GameInfo for networking

--- core/src/com/haanthony/GameInfo.java
+++ core/src/com/haanthony/GameInfo.java
@@ -1,5 +1,7 @@
 package com.haanthony;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import com.haanthony.Game.GameColor;
@@ -25,5 +27,71 @@ public class GameInfo {
 
 	public int getDiceRoll() {
 		return diceRoll;
+	}
+	
+	private static final char HEADER = '%';
+	private static final char SEPERATER = '^';
+	private static final char END_INDICATER = '&';
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(HEADER);
+		sb.append(color.toString()).append(SEPERATER);
+		
+		for (Choice choice : choices) {
+			sb.append(choice).append(SEPERATER);
+		}
+		
+		sb.append(diceRoll).append(END_INDICATER);
+		return sb.toString();
+	}
+	
+	public static GameInfo fromString(String gameInfoString) {
+		// Process header
+		if (gameInfoString.charAt(0) != HEADER) {
+			throw new IllegalArgumentException("Header not found");
+		}
+		
+		GameInfoBuilder builder = new GameInfoBuilder();
+		
+		// Process color
+		int index = 1;
+		
+		int nextSeperater = gameInfoString.indexOf(SEPERATER, index);
+		builder.color = GameColor.valueOf(gameInfoString.substring(index, nextSeperater));
+		index = nextSeperater + 1;
+		
+		// Process choices
+		builder.choices = new HashSet<Choice>();
+		
+		nextSeperater = gameInfoString.indexOf(SEPERATER, index);
+		while (nextSeperater != -1) {
+			String choiceString = gameInfoString.substring(index, nextSeperater);
+			Choice choice = Choice.fromString(choiceString);
+			builder.choices.add(choice);
+			index = nextSeperater + 1;
+			nextSeperater = gameInfoString.indexOf(SEPERATER, index);
+		}
+		
+		// Process dice roll and end indicater
+		nextSeperater = gameInfoString.indexOf(END_INDICATER, index);
+		if (nextSeperater == -1) {
+			throw new IllegalArgumentException("No end indicater on this game info string");
+		}
+		builder.diceRoll = Integer.valueOf(gameInfoString.substring(index, nextSeperater));
+		
+		return builder.build();
+	}
+	
+	private static class GameInfoBuilder {
+		private GameColor color;
+		private Set<Choice> choices;
+		private int diceRoll;
+		
+		public GameInfo build() {
+			return new GameInfo(color, choices, diceRoll);
+		}
 	}
 }
